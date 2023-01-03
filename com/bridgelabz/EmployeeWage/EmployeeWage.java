72220bbde182242b2e18f70e46526b54ea658c2c
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-04 01:18:58 +0530

    Added: Checking Employee Attendance

--- com/bridgelabz/EmployeeWage/EmployeeWage.java
+++ com/bridgelabz/EmployeeWage/EmployeeWage.java
@@ -1,8 +1,18 @@
 package com.bridgelabz.EmployeeWage;
 
+import java.util.Random;
+
 public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Java Program");
+
+		Random random = new Random();
+		int randomNum = random.nextInt(2);
+
+		if (randomNum == 1)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 
 }
