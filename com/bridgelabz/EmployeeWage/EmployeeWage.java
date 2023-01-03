e4b55c0651415bd4276bab7add336ec1a2b4d7cd
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-04 01:14:06 +0530

    Added: Welcome message

---
+++ com/bridgelabz/EmployeeWage/EmployeeWage.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.EmployeeWage;
+
+public class EmployeeWage {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Java Program");
+	}
+
+}
