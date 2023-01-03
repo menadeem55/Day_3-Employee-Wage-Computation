2d7f0a5ca21011925ef1f690e4b503191fc1a218
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-04 02:22:13 +0530

    Added: calculate wages for a month

--- com/bridgelabz/EmployeeWage/EmployeeWage.java
+++ com/bridgelabz/EmployeeWage/EmployeeWage.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class EmployeeWage {
 	/*
-	 * Add Part time Employee & Wage by assuming part time hours is 8
+	 * Calculating Wages for a Month by assuming 20 working day per month
 	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Java Program");
@@ -18,34 +18,46 @@ public class EmployeeWage {
 
 		int halfDayHrs = 4;
 
-		int salary = 0; // will give 0,1,2 random number
+		int salary = 0;
 
-		int randomNum = random.nextInt(3);
+		int monthlysalary = 0;
+
+		int workingDaysPerMonth = 20;
 
 		/*
-		 * By using Switch case statement Calculate and getting this salary Getting this
-		 * salary employee Present and Absent and print salary
+		 * using a loop to ask how many salary in working day per monthly will give
+		 * 0,1,2 random number
 		 */
+		for (int i = 1; i <= workingDaysPerMonth; i++) {
+			int randomNum = random.nextInt(3);
 
-		switch (randomNum) {
+			/*
+			 * By using Switch case statement Calculate and getting this salary Getting this
+			 * salary employee Present and Absent and print salary
+			 */
+			switch (randomNum) {
 
-		case 0:
-			System.out.println("Employee is Absent");
-			System.out.println("Monthly Salary will be: ");
-			break;
-		case 1:
-			System.out.println("Employee is Present");
-			System.out.println("Monthly salary for Full Day: ");
-			salary = wagePerHrs * fullDayHrs;
-			break;
-		case 2:
-			System.out.println("Employee is Present");
-			System.out.println("Monthly Salary for Half Day: ");
-			salary = wagePerHrs * halfDayHrs;
-			break;
+			case 0:
+				System.out.println("Employee is Absent");
+				System.out.println("Monthly Salary will be: ");
+				break;
+			case 1:
+				System.out.println("Employee is Present");
+				System.out.println("Monthly salary for Full Day: ");
+				salary = wagePerHrs * fullDayHrs;
+				break;
+			case 2:
+				System.out.println("Employee is Present");
+				System.out.println("Monthly Salary for Half Day: ");
+				salary = wagePerHrs * halfDayHrs;
+				break;
+
+			}
+			monthlysalary = monthlysalary + salary;
+			System.out.println(+salary);
 
 		}
-		System.out.println(+salary);
+		System.out.println("Monthly Salary: " + monthlysalary); // Print the total of monthly salary
 
 	}
 
